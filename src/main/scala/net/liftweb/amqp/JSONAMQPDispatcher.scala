788073dfdf3c811fa366c20780ee95bc34bfe8fb
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2009-05-27 12:58:51 -0700

    Support for logging

--- src/main/scala/net/liftweb/amqp/JSONAMQPDispatcher.scala
+++ src/main/scala/net/liftweb/amqp/JSONAMQPDispatcher.scala
@@ -18,11 +18,21 @@ import _root_.java.util.TimerTask
 import _root_.com.thoughtworks.xstream.XStream
 import _root_.com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver
 
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
+
+import org.apache.log4j.Logger
+import org.apache.log4j.PropertyConfigurator
+import java.util.Properties
+import java.io.FileInputStream
+import java.io.IOException
+
 class JSONSerializedAMQPDispatcher[T](
   factory: ConnectionFactory,
   host: String,
   port: Int
-) extends AMQPDispatcher[T](factory, host, port) {
+) extends AMQPDispatcher[T](factory, host, port) {  
   override def configure(channel: Channel) {
     // Get the ticket.
     val ticket = channel.accessRequest("/data")
@@ -32,7 +42,7 @@ class JSONSerializedAMQPDispatcher[T](
     channel.queueBind(ticket, "mult_queue", "mult", "routeroute")
     // Use the short version of the basicConsume method for convenience.
     channel.basicConsume(ticket, "mult_queue", false, new SerializedConsumer(channel, this))
-  }
+  }  
 }
 
 trait JSONHandler {
@@ -41,7 +51,62 @@ trait JSONHandler {
   }
 }
 
+trait JSONToSQLHandler {
+  var _emf : Option[EntityManagerFactory] = None
+  def entityMgrFactory( db : String ) : EntityManagerFactory = {
+    _emf match {
+      case Some( emf ) => emf
+      case None => {
+	val emf = Persistence.createEntityManagerFactory( db )
+	_emf = Some( emf )
+	emf
+      }
+    }
+  }
+  var _em : Option[EntityManager] = None
+  def entityManager( db : String ) : EntityManager = {
+    _em match {
+      case Some( em ) => em
+      case None => {
+	val em = entityMgrFactory( db ).createEntityManager()
+	_em = Some( em )
+	em
+      }
+    }
+  }
+  def handle( db : String )( contents: String ) : Unit = {
+    var obj : java.lang.Object = null;
+    try {
+      obj = 
+	new XStream(
+	  new JettisonMappedXmlDriver()
+	).fromXML( contents );
+      
+      try {
+	entityManager( db ).getTransaction().begin();
+	entityManager( db ).persist( obj );
+	entityManager( db ).getTransaction().commit();
+      }
+      catch {
+	case e => {
+	  println( "persistence error" )
+	  e.printStackTrace
+	}
+      }
+    }
+    catch {
+      case e => {
+	  println( "marshaling error" )
+	  e.printStackTrace
+	}
+    }
+  }
+}
+
 class JSONAMQPListener {
+  val LOG_PROPERTIES_FILE : String =
+    "src/main/resources/Log4J.properties";  
+
   val params = new ConnectionParameters
   params.setUsername("guest")
   params.setPassword("guest")
@@ -56,19 +121,46 @@ class JSONAMQPListener {
       "localhost",
       5672
     )
+
+  def configureLogging() {
+    val logProperties : Properties = new Properties();
+    val log : Logger =
+      Logger.getLogger(classOf[JSONAMQPListener]);
+    try {      
+      logProperties.load(new FileInputStream(LOG_PROPERTIES_FILE));
+      PropertyConfigurator.configure(logProperties);
+      log.info("Logging initialized.");
+    }
+    catch {
+      case e => e.printStackTrace
+    }
+  }
+  
+  def testHandle = {
+    val jal = new net.liftweb.amqp.JSONAMQPListener
+    jal.amqp ! net.liftweb.amqp.AMQPReconnect( 4 )
+    jal.jsonListener.handle( "stockholm" )(
+      new com.thoughtworks.xstream.XStream(
+	new com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver
+      ).toXML(
+	(new com.biosimilarity.reflection.model.REPL).read( "lambda x.x" )
+      )
+    )
+  }
+
   amqp.start
 
   // JSON Listener
   class JSONListener( logging : Boolean )
   extends Actor
-  with JSONHandler {
+  with JSONToSQLHandler {
     def act = {
       react {
 	case msg@AMQPMessage( contents : String ) => {
 	  if ( logging ) {
 	    println("received: " + msg)
 	  };
-	  handle( contents );
+	  handle( "stockholm" )( contents );
 	  act
 	}
       }
